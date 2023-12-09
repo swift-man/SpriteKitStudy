49de67ea9c0fe544857643e5131584000e4a4820
swift-man <jiniopening@naver.com>
2023-12-10 00:19:07 +0900

    player 추가

--- SpriteKitStudy/Chapter 3/Chapter3Scene.swift
+++ SpriteKitStudy/Chapter 3/Chapter3Scene.swift
@@ -12,7 +12,7 @@ class Chapter3Scene: SKScene {
   
   let cam = SKCameraNode()
   
-  let bee = SKSpriteNode()
+  let player = Player()
   
   let ground = Ground()
   
@@ -22,8 +22,6 @@ class Chapter3Scene: SKScene {
     self.backgroundColor = UIColor(red: 0.4, green: 0.6, blue: 0.95, alpha: 1.0)
     
     self.camera = cam
-    
-    self.addTheFlyingBee()
     
     let bee2 = Bee()
     bee2.position = CGPoint(x: 325, y: 325)
@@ -38,49 +36,12 @@ class Chapter3Scene: SKScene {
     ground.size = CGSize(width: self.size.width * 6, height: 0)
     ground.createChildren()
     self.addChild(ground)
+    
+    player.position = CGPoint(x: 150, y: 250)
+    self.addChild(player)
   }
   
   override func didSimulatePhysics() {
-    self.camera!.position = bee.position
-  }
-  
-  func addTheFlyingBee() {
-    bee.size = CGSize(width: 28, height: 24)
-    bee.position = CGPoint(x: 250, y: 250)
-    self.addChild(bee)
-    
-    let beeAtlas = SKTextureAtlas(named: "Enemies")
-    
-    let beeFrames: [SKTexture] = [
-      beeAtlas.textureNamed("bee"),
-      beeAtlas.textureNamed("bee-fly")
-    ]
-    
-    let flyAction = SKAction.animate(with: beeFrames,
-                                     timePerFrame: 0.14)
-    
-    let beeAction = SKAction.repeatForever(flyAction)
-    bee.run(beeAction)
-    
-    let pathLeft = SKAction.moveBy(x: -200,
-                                   y: -10,
-                                   duration: 2)
-    let pathRight = SKAction.moveBy(x: 200,
-                                    y: 10,
-                                    duration: 2)
-    
-    let flipTextureNegative = SKAction.scaleX(to: -1, duration: 0)
-    let flipTexturePositive = SKAction.scaleX(to: 1, duration: 0)
-    
-    let flightOfTheBee = SKAction.sequence([
-      pathLeft,
-      flipTextureNegative,
-      pathRight,
-      flipTexturePositive
-    ])
-    
-    let neverEndingFlight = SKAction.repeatForever(flightOfTheBee)
-    
-    bee.run(neverEndingFlight)
+    self.camera!.position = player.position
   }
 }
---
+++ SpriteKitStudy/Chapter 3/Player.swift
@@ -0,0 +1,72 @@
+//
+//  Player.swift
+//  SpriteKitStudy
+//
+//  Created by SwiftMan on 12/10/23.
+//
+
+import Foundation
+import SpriteKit
+
+class Player: SKSpriteNode, GameSprite {
+  var textureAtlas = SKTextureAtlas(named: "Pierre")
+  
+  var initialSize = CGSize(width: 64, height: 64)
+  
+  var flyAnimation = SKAction()
+  var soarAnimation = SKAction()
+  
+  init() {
+    super.init(texture: nil,
+               color: .clear,
+               size: initialSize)
+    
+    createAnimations()
+    self.run(flyAnimation, withKey: "flapAnimation")
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+  
+  func createAnimations() {
+    let rotateUpAction = SKAction.rotate(toAngle: 0, duration: 0.475)
+    rotateUpAction.timingMode = .easeOut
+    
+    let rotateDownAction = SKAction.rotate(toAngle: -1, duration: 0.8)
+    rotateDownAction.timingMode = .easeOut
+    
+    let flyFrames: [SKTexture] = [
+      textureAtlas.textureNamed("pierre-flying-1"),
+      textureAtlas.textureNamed("pierre-flying-2"),
+      textureAtlas.textureNamed("pierre-flying-3"),
+      textureAtlas.textureNamed("pierre-flying-4"),
+      textureAtlas.textureNamed("pierre-flying-3"),
+      textureAtlas.textureNamed("pierre-flying-2"),
+    ]
+    
+    let flyAction = SKAction.animate(with: flyFrames, timePerFrame: 0.03)
+    
+    flyAnimation = SKAction.group([
+      SKAction.repeatForever(flyAction),
+      rotateUpAction
+    ])
+    
+    let soarFrames: [SKTexture] = [textureAtlas.textureNamed("pieree-flying-1")]
+    
+    let soarAction = SKAction.animate(with: soarFrames, timePerFrame: 1)
+    
+    soarAnimation = SKAction.group([
+      SKAction.repeatForever(soarAction),
+      rotateDownAction
+    ])
+  }
+  
+  
+  
+  func onTap() {
+    
+  }
+  
+  
+}
